5711bc8020e8cef0f680b153f37c2abb0d369c0a
alexdgarland <alex.d.garland@gmail.com>
2014-01-14 23:20:32 +0000

    Create folder for part 3 with C# examples
    Use of Neo4jClient in C# to add nodes and relationships, then do a
    simple query and check/ display results

---
+++ Part 3 - Talking To The Graph/EnterpriseWidgetManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using Neo4jClient;          //Add by using Nuget Package Manager ("Install-Package Neo4jClient")
+
+namespace Neo4jWidgetManager
+{
+
+
+    class EnterpriseWidgetManager
+    {
+	
+        static void Main(string[] args)
+        {
+            // Set up connection
+            var client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+            client.Connect();
+
+            //Clear down database of widgets, components and usage relationships
+            ClearDown(client);
+
+            // Create widgets
+            var widgetA = new WidgetNode("WidgetA", client);
+            var widgetB = new WidgetNode("WidgetB", client);
+            Console.WriteLine("Widgets Created");
+
+            // Create components
+            var Component1 = new ComponentNode("Component1", client);
+            var Component2 = new ComponentNode("Component2", client);
+            var Component3 = new ComponentNode("Component3", client);
+            var Component4 = new ComponentNode("Component4", client);
+            Console.WriteLine("Components Created");
+
+            // Create relationships...
+            // ... for Widget A
+            widgetA.AddComponent(Component1);
+            widgetA.AddComponent(Component2);
+            widgetA.AddComponent(Component3);
+            // ... for Widget B
+            widgetB.AddComponent(Component2);
+            widgetB.AddComponent(Component3);
+            widgetB.AddComponent(Component4);
+            Console.WriteLine("Relationships Created\n");
+
+            // Query - find components used in both widgets
+            IEnumerable sharedComponents = WidgetNode.GetSharedComponents(widgetA, widgetB, client);
+            if (sharedComponents.GetEnumerator().MoveNext() == false)
+            {
+                Console.WriteLine("No shared components identified");
+            }
+            else
+            {
+                Console.WriteLine("The following components are used by both {0} and {1}:\n", widgetA.Name, widgetB.Name);
+                foreach (string c in sharedComponents)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+            Console.WriteLine();
+        
+        }
+
+        static void ClearDown(GraphClient client)
+        {
+            // Deletes everything (assuming that all nodes have some usage relationship)
+            client.Cypher
+                .Match("(w:Widget)-[r:USED_IN]-(c:Component)")
+                .Delete("r, w, c")
+                .ExecuteWithoutResults();
+        }
+
+    }
+	
+	
+}
---
+++ Part 3 - Talking To The Graph/NamedNode.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using Neo4jClient;          // Add by using Nuget Package Manager ("Install-Package Neo4jClient")
+
+namespace Neo4jWidgetManager
+{
+
+
+    abstract class NamedNode
+    {
+        public string Name { get; private set; }
+        protected GraphClient client;
+
+        public NamedNode(string Name, GraphClient client)
+        {
+            this.Name = Name;
+            this.client = client;
+            createGraphNode();
+        }
+
+        private void createGraphNode()
+        {
+            client.Cypher
+                .Create("(n: " + NodeType + " {Name: { NodeName }})")
+                .WithParam("NodeName", this.Name)
+                .ExecuteWithoutResults();
+        }
+
+        public abstract string NodeType { get; }
+    }
+
+	
+    class WidgetNode : NamedNode
+    {
+        public WidgetNode(string Name, GraphClient client) : base(Name, client) { }
+        public override string NodeType { get { return "Widget"; } }
+
+        public void AddComponent(ComponentNode component)
+        {
+            client.Cypher
+                .Match("(c:Component)", "(w:Widget)")
+                .Where((ComponentNode c) => c.Name == component.Name)
+                .AndWhere((WidgetNode w) => w.Name == this.Name)
+                .Create("c-[:USED_IN]->w")
+                .ExecuteWithoutResults();
+        }
+
+        public static IEnumerable GetSharedComponents(WidgetNode widget1, WidgetNode widget2, GraphClient client)
+        {
+            return client.Cypher
+                    .Match("(w1{Name:{name1}})<-[:USED_IN]-(c:Component)-[:USED_IN]->(w2{Name:{name2}})")
+                    .WithParam("name1", widget1.Name)
+                    .WithParam("name2", widget2.Name)
+                    .Return(c => c.As<ComponentResult>().Name)
+                    .Results;
+        }
+
+        private class ComponentResult
+        {
+            public string Name;
+        }
+    }
+
+
+
+    class ComponentNode : NamedNode
+    {
+        public ComponentNode(string Name, GraphClient client) : base(Name, client) { }
+        public override string NodeType { get { return "Component"; } }
+
+    }
+	
+	
+}
